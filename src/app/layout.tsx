01acc908562cc7399c78f9b8b4e2ed15d21228a3
verdalyn <elangovan.21ad+github@gmail.com>
2024-10-19 20:10:14 +0530

    Dev (#12)
    * added tracker
    
    * fix tailwind missing
    
    * add
    
    * gtag
    
    ---------
    
    Co-authored-by: macbook <macbook@MacBook-Air-2.local>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import GoogleAnalytics from '../components/analytics'
-
+import { GoogleTagManager } from '@next/third-parties/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <GoogleAnalytics />
+       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_MEASUREMENT_ID || ''} />
       <body>{children}</body>
     </html>
   )
